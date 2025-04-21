511b2e0019c4543e14b1cc78bb50d03d9cc5b2ef
Post Provincial <postcolonialwiki@gmail.com>
2025-04-21 22:21:59 +0200

    all

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversedInput = input.split('').reverse().join('');
+    return reversedInput
 };
 
 // Do not edit below this line
