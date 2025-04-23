4df72e52a4112f5163f1433857c281dcb398e8ce
Post Provincial <postcolonialwiki@gmail.com>
2025-04-23 20:01:37 +0200

    all

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...valuesToRemove){
+    let result = []
+
+    for(element of arr){
+        if(!valuesToRemove.includes(element)){
+            result.push(element);
+        }
+    }
+
+    return result
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
