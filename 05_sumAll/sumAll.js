dbb54ff35b6d241ee7e642543c1518a575930b54
Post Provincial <postcolonialwiki@gmail.com>
2025-04-24 17:24:36 +0200

    all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstValue, secondValue) {
+    let sum = 0;
 
+    if(!(Number.isInteger(firstValue) && Number.isInteger(secondValue) && firstValue > 0 & secondValue > 0)){
+        return 'ERROR';
+    }
+
+
+    const passedValues = [firstValue, secondValue];
+    let smallest = Math.min(...passedValues);
+    let largest = Math.max(...passedValues);
+
+
+    for(i = smallest; i <= largest; i++){
+            sum += i;
+        }
+     
+    return sum;
 };
 
 // Do not edit below this line
