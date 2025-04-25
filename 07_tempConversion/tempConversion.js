36090005e9c335714b8cccf1352c4aca99c73d40
Post Provincial <postcolonialwiki@gmail.com>
2025-04-26 00:34:35 +0200

    all

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(valueInF) {
+  const exact = (valueInF - 32) * (5/9);
+  const rounded = Math.round(exact * 10) / 10; // Best method
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valueinC) {
+  const exact = (valueinC *(9/5)) + 32;
+  const rounded = Math.round(exact * 10) / 10; // Best method
+  return rounded;
 };
 
 // Do not edit below this line
