dc6b7bcdccfa9a1b7d76921cd784a15f7d920e5c
Post Provincial <postcolonialwiki@gmail.com>
2025-04-26 17:23:52 +0200

    all

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function(value) {
+    // Remove all non-alphanumeric characters (including punctuation AND spaces)
+    let cleanStr = value.replace(/[^a-z0-9]/gi, '');
+    // Convert to lowercase
+    cleanStr = cleanStr.toLowerCase();
+    
+    // Reverse and compare
+    let reversedStr = cleanStr.split('').reverse().join('');
+    
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
